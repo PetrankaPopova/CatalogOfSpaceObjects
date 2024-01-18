9a4fcbee6b86cff3340e65389e114e48b312d42e
Pepi <petranka_popova_@abv.bg>
2024-01-18 10:54:56 +0200

    added logic for PlanetBean

--- JSF_HelloWorld/src/main/java/com/journaldev/jsf/beans/Welcome.java
+++ JSF_HelloWorld/src/main/java/com/journaldev/jsf/beans/Welcome.java
@@ -4,9 +4,9 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-@ManagedBean(name = "helloWorld")
+@ManagedBean(name = "welcome")
 @SessionScoped
-public class HelloWorld implements Serializable {
+public class Welcome implements Serializable {
 
 	private static final long serialVersionUID = -6913972022251814607L;
 
